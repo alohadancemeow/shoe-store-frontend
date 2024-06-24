77997c4aaeb4c9feb7e45215e9c953a67a3ad5c6
Alohadancemeow <nawongsak@gmail.com>
2024-06-24 17:00:20 +0700

    🍀 fixed home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,15 @@ import HeroBanner from "@/components/HeroBanner";
 import ProductCard from "@/components/ProductCard";
 import Wrapper from "@/components/Wrapper";
 
-// import { fetchDataFromApi } from "@/utils/api";
+import { fetchDataFromApi } from "@/utils/api";
 
 const Home = async () => {
-  const products: any = [];
+  const products = await fetchDataFromApi("/products?populate=*");
 
   return (
     <main>
       <HeroBanner />
       <Wrapper>
-        {/* heading and paragaph start */}
         <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
           <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
             Cushioning for Your Miles
@@ -22,35 +21,15 @@ const Home = async () => {
             running.
           </div>
         </div>
-        {/* heading and paragaph end */}
 
-        {/* products grid start */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
           {products?.data?.map((product: any) => (
-            <ProductCard key={product?.id} data={product} />
+            <ProductCard key={product?.id} prodcut={product} />
           ))}
-          {/* <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard /> */}
         </div>
-        {/* products grid end */}
       </Wrapper>
     </main>
   );
 };
-
-// export async function getStaticProps() {
-//   const products = await fetchDataFromApi("/api/products?populate=*");
-
-//   return {
-//     props: { products },
-//   };
-// }
 
 export default Home;
--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,44 +1,49 @@
-import { getDiscountedPricePercentage } from "@/utils/helper";
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
-const ProductCard = ({ data: { attributes: p, id } }) => {
-    return (
-        <Link
-            href={`/product/${p.slug}`}
-            className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer"
-        >
-            <Image
-                width={500}
-                height={500}
-                src={p.thumbnail.data.attributes.url}
-                alt={p.name}
-            />
-            <div className="p-4 text-black/[0.9]">
-                <h2 className="text-lg font-medium">{p.name}</h2>
-                <div className="flex items-center text-black/[0.5]">
-                    <p className="mr-2 text-lg font-semibold">
-                        &#8377;{p.price}
-                    </p>
+"use client";
 
-                    {p.original_price && (
-                        <>
-                            <p className="text-base  font-medium line-through">
-                                &#8377;{p.original_price}
-                            </p>
-                            <p className="ml-auto text-base font-medium text-green-500">
-                                {getDiscountedPricePercentage(
-                                    p.original_price,
-                                    p.price
-                                )}
-                                % off
-                            </p>
-                        </>
-                    )}
-                </div>
-            </div>
-        </Link>
-    );
+import { getDiscountedPricePercentage } from "@/utils/helper";
+import Link from "next/link";
+
+type Props = {
+  prodcut?: any;
+};
+
+const ProductCard = ({ prodcut }: Props) => {
+  return (
+    <Link
+      href={`/product/${prodcut?.attributes?.slug}`}
+      className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer"
+    >
+      <img
+        width={500}
+        height={500}
+        src={`${process.env.NEXT_PUBLIC_UPLOAD_URL}${prodcut.attributes?.thumbnail.data.attributes.url}`}
+        alt={prodcut.attributes?.name}
+      />
+      <div className="p-4 text-black/[0.9]">
+        <h2 className="text-lg font-medium">{prodcut?.attributes?.name}</h2>
+        <div className="flex items-center text-black/[0.5]">
+          <p className="mr-2 text-lg font-semibold">
+            &#8377;{prodcut?.attributes?.price}
+          </p>
+
+          {prodcut?.attributes?.original_price && (
+            <>
+              <p className="text-base  font-medium line-through">
+                &#8377;{prodcut?.attributes?.original_price}
+              </p>
+              <p className="ml-auto text-base font-medium text-green-500">
+                {getDiscountedPricePercentage(
+                  prodcut?.attributes?.original_price,
+                  prodcut?.attributes?.price
+                )}
+                % off
+              </p>
+            </>
+          )}
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 export default ProductCard;
